ec0b61f628542160d2062d7c7496f71747be17c1
o m <sandrm@ukr.net>
2017-03-04 19:46:11 +0200

    Seccond commit

--- src/Demo.scala
+++ src/Demo.scala
@@ -9,10 +9,27 @@ class FirstBean(val anyText: String){
 
 
 object Demo {
+  var peremMutable: String = "Peremennaya 1 - mutable"; 
+  val peremImMutable: String = "Peremennaya 2 - immutable";
+  
   def main(args: Array[String]){
     println("Hello Scala !");
     
     val firstBean = new FirstBean("my text");
     firstBean.firstMethod("other Text");
+    
+    println("Print " + peremMutable);
+    println("Print " + peremImMutable);
+    
+    printVariable("A1","A2");
+    printVariable(peremMutable, peremImMutable);
+    peremMutable = "Peremennaya 1 - changed";
   }
+
+  def printVariable(p1: String, p2: String){
+    println("Print p1=" + p1);
+    println("Print p2=" + p2);
+  }
+
+
 }
